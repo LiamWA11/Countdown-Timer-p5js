26c17f019251b2aced5ad4fe2cd0d1fbd0ee8abe
Liam Angus <angusliam5@gmail.com>
2018-06-20 09:20:09 +1000

    Added basic template files

---
+++ sketch.js
@@ -0,0 +1,13 @@
+function setup() {
+    createCanvas(200, 200);
+  // put setup code here
+}
+
+function draw() {
+    background(200, 0, 255);
+    line(20, 30, 10, 50)
+    line(20, 30, 30, 50)
+    line(10, 50, 30, 50)
+
+  // put drawing code here
+}
